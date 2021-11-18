50d0a7f14045084470279bc513677d86d5addbb6
anon <sapling@imissfauna.com>
2021-11-17 17:19:41 -0800

    handle pending streams

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Head from "next/head"
-import { pollLivestreamStatus } from "../server/livestream_poller"
+import { STREAM_STATUS, pollLivestreamStatus } from "../server/livestream_poller"
 import { ERROR_IMAGE_SET, HAVE_STREAM_IMAGE_SET, NO_STREAM_IMAGE_SET } from "../imagesets"
 
 function selectRandomImage(fromSet) {
@@ -10,10 +10,15 @@ function selectRandomImage(fromSet) {
 export async function getServerSideProps({ req, res }) {
     if (process.env.USE_DUMMY_DATA === "true") {
         return { props: {
-            isLive: false,
+            status: STREAM_STATUS.OFFLINE,
             isError: false,
-            videoLink: "https://www.youtube.com/watch?v=aaaaaaaaaaa",
-            videoTitle: "Dummy dummy dummy dummy",
+            streamInfo: { }
+            // streamInfo: {
+            //     link: "https://www.youtube.com/watch?v=aaaaaaaaaaa",
+            //     title: "Dummy dummy dummy dummy",
+            //     startTime: (new Date()).getTime() + 3600,
+            //     currentTime: (new Date()).getTime()
+            // }
         } }
     }
 
@@ -22,47 +27,67 @@ export async function getServerSideProps({ req, res }) {
     const { error, result } = await pollLivestreamStatus(process.env.WATCH_CHANNEL_ID)
     if (error) {
         console.warn("livestream poll returned error:", error)
-        return { props: { isLive: false, isError: true } }
+        return { props: { isError: true } }
     }
 
     return { props: {
-        isLive: result.live,
+        status: result.live,
         isError: false,
-        videoLink: result.videoLink,
-        videoTitle: result.title,
+        streamInfo: {
+            link: result.videoLink,
+            title: result.title,
+            startTime: result.streamStartTime?.getTime() || null,
+            currentTime: (new Date()).getTime()
+        }
     } }
 }
 
+function StreamInfo(props) {
+    let link, text
+    if (props.info.link) {
+        switch (props.status) {
+            case STREAM_STATUS.LIVE:
+                text = "Current Stream: "
+                break
+            case STREAM_STATUS.STARTING_SOON:
+                text = "Starting Soon: "
+                break
+            default:
+                text = "Next Stream: "
+                break
+        }
+
+        link = <b><a href={props.info.link}>{props.info.title}</a></b>
+    } else {
+        text = "Current Stream: "
+        link = <><b>NOTHING UUUUUUUuuuuuu</b> <small>(but maybe there&apos;s a member stream...)</small></>
+    }
+
+    return <div className="stream-info">
+        <p>{text} {link}</p>
+    </div>
+}
+
 export default function Home(props) {
-    let className, caption, image, bottomInfo
+    let className, caption = "", image, bottomInfo
     const channelLink = `https://www.youtube.com/channel/${process.env.WATCH_CHANNEL_ID}`
 
-    if (props.isLive) {
-        className = "comfy" 
-        caption = "I Don't Miss Fauna"
-        image = selectRandomImage(HAVE_STREAM_IMAGE_SET)
-        bottomInfo = <>
-            {"Current Stream: "}
-            <a href={props.videoLink}><b>{props.videoTitle}</b></a>
-        </>
-    } else if (props.isError) {
+    if (props.isError) {
         className = "error"
-        caption = "MEOW!"
         image = selectRandomImage(ERROR_IMAGE_SET)
-        bottomInfo = <>
-            {"There was a problem checking stream status. "}
-            <a href={channelLink}>You can check Fauna&apos;s channel yourself</a>!
-        </>
-    } else {
+        bottomInfo = <div className="stream-info">
+            <p>There was a problem checking stream status. <a href={channelLink}>You can check Fauna&apos;s channel yourself</a>!</p>
+        </div>
+    } else if (props.status != STREAM_STATUS.LIVE && props.status != STREAM_STATUS.STARTING_SOON) {
         className = "miss-her"
         caption = ""
         image = selectRandomImage(NO_STREAM_IMAGE_SET)
-        bottomInfo = <>
-            {"Current Stream: "}
-            <b>NOTHING UUUUUUUuuuuu</b> 
-            {" "}
-            <small>(maybe there&apos;s a member stream...)</small>
-        </>
+        bottomInfo = <StreamInfo status={props.status} info={props.streamInfo} />
+    } else {
+        className = "comfy" 
+        caption = "I Don't Miss Fauna"
+        image = selectRandomImage(HAVE_STREAM_IMAGE_SET)
+        bottomInfo = <StreamInfo status={props.status} info={props.streamInfo} />
     }
 
     return <div className={styles.site}>
@@ -75,9 +100,7 @@ export default function Home(props) {
             <h1>{caption}</h1>
             <img src={image} alt="wah" />
 
-            <div className="stream-info">
-                <p>{bottomInfo}</p>
-            </div>
+            {bottomInfo}
 
             <footer>
                 <a href={channelLink}>Ceres Fauna Ch. hololive-EN</a> <br />
--- server/livestream_poller.js
+++ server/livestream_poller.js
@@ -1,5 +1,12 @@
 import { parse } from "node-html-parser"
 
+export const STREAM_STATUS = {
+    OFFLINE: 1,
+    INDETERMINATE: 2,
+    STARTING_SOON: 3,
+    LIVE: 4,
+}
+
 function createPollRoute(channelID) {
     return `https://www.youtube.com/channel/${channelID}/live`
 }
@@ -10,44 +17,95 @@ function validateVideoLink(anyLink) {
     }
 }
 
-export async function pollLivestreamStatus(channelID) {
-    let youtubeHTML
+export async function fetchLivestreamPage(channelID) {
     try {
-        const res = await fetch(createPollRoute(channelID), {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-            }
-        })
+        const res = await fetch(createPollRoute(channelID))
         if (res.status !== 200) {
-            return {error: `HTTP status: ${res.status}`, result: null}
+            return { error: `HTTP status: ${res.status}`, result: null }
         }
-        youtubeHTML = await res.text()
-
-        console.warn(res.statusText)
-    } catch(e) {
-        return {error: e.toString(), result: null}
+        const youtubeHTML = await res.text()
+        return { error: null, result: youtubeHTML }
+    } catch (e) {
+        return { error: e.toString(), result: null }
     }
+}
 
-    const dom = parse(youtubeHTML, {
+const VIDEO_INFO_EXPECT_START = "var ytInitialPlayerResponse = "
+export function extractLivestreamInfo(fromPageContent) {
+    const dom = parse(fromPageContent, {
         blockTextElements: {
-            script: false,
+            script: true,
             noscript: false,
             style: false,
             pre: false,
         }
     })
 
-
     const canonical = dom.querySelector("link[rel='canonical']")
     if (!canonical) {
-        return {error: `Malformed HTML`, result: null} 
+        return { error: "Malformed HTML", result: null } 
     }
 
     const videoLink = validateVideoLink(canonical.getAttribute("href"))
-    if (videoLink) {
-        const liveTitle = dom.querySelector("meta[name='title']").getAttribute("content")
-        return {error: null, result: {live: true, title: liveTitle, videoLink}}
+    if (!videoLink) {
+        return { error: null, result: { live: STREAM_STATUS.OFFLINE, title: null, videoLink: null, streamStartTime: null } }
     } 
 
-    return {error: null, result: {live: false, title: null, videoLink: null}}
+    const liveTitle = dom.querySelector("meta[name='title']").getAttribute("content")
+    const basicResponse = { error: null, result: { live: STREAM_STATUS.INDETERMINATE, title: liveTitle, videoLink, streamStartTime: null } }
+
+    const scripts = dom.querySelectorAll("script")
+    let playerInfo = null
+    for (let i = 0; i < scripts.length; ++i) {
+        const text = scripts[i].textContent
+        
+        if (text.startsWith(VIDEO_INFO_EXPECT_START)) {
+            console.log(text.substring(VIDEO_INFO_EXPECT_START.length, text.length - 1))
+            try {
+                playerInfo = JSON.parse(text.substring(VIDEO_INFO_EXPECT_START.length, text.length - 1))
+            } catch {
+                continue
+            }
+            break
+        }
+    }
+
+    if (!playerInfo) {
+        return basicResponse
+    }
+
+    // Check if stream frame is actually live, or just a waiting room
+    const videoDetails = playerInfo.videoDetails
+    if (videoDetails?.isLiveContent && videoDetails?.isUpcoming) {
+        basicResponse.result.live = STREAM_STATUS.STARTING_SOON
+    } else if (videoDetails?.isLiveContent && !videoDetails?.isUpcoming) {
+        basicResponse.result.live = STREAM_STATUS.LIVE
+    }
+
+    // Check stream frame start time
+    // If it's more than one hour out, act as if it was offline
+    const ts = playerInfo.playabilityStatus?.
+        liveStreamability?.liveStreamabilityRenderer?.offlineSlate?.
+        liveStreamOfflineSlateRenderer?.scheduledStartTime
+    if (ts === undefined) {
+        return basicResponse
+    }
+
+    const expectedStartTime = parseInt(ts) * 1000
+    const waitTimeLeftMS = expectedStartTime - (new Date().getTime())
+    basicResponse.result.streamStartTime = new Date(expectedStartTime)
+    if (waitTimeLeftMS > 3600 * 1000) {
+        basicResponse.result.live = STREAM_STATUS.OFFLINE
+    }
+
+    return basicResponse
+}
+
+export async function pollLivestreamStatus(channelID) {
+    const { error, result: youtubeHTML } = await fetchLivestreamPage(channelID)
+    if (error) {
+        return { error, result: null }
+    }
+
+    return extractLivestreamInfo(youtubeHTML)   
 }
